dc6069b2a030ef48e8247b35807ed91c2731c1fb
MichaelElten <elten.michael@gmail.com>
2021-01-30 22:01:16 -0500

    add_template_Rshiny

--- code/02_processing.R
+++ code/02_processing.R
@@ -6,9 +6,16 @@ library(tidyverse)
 library(googlesheets4)
 
 #Import data ----
+#perhaps just want to refresh the dataset from live version only occasionally
 
-SS<-"https://docs.google.com/spreadsheets/d/1Y0Odg0ar_YsraYrcQ4r-oJJzSDpIK97SkLBv0yhRMWk/edit#gid=1796417297"
-data_import<-googlesheets4::read_sheet(SS, sheet = "full_dataset")
+# SS<-"https://docs.google.com/spreadsheets/d/1Y0Odg0ar_YsraYrcQ4r-oJJzSDpIK97SkLBv0yhRMWk/edit#gid=1796417297"
+# data_import<-googlesheets4::read_sheet(SS, sheet = "full_dataset")
+# fix_data_import <-data_import %>%
+# mutate(across(where(is.list),as.character))
+# write_csv(fix_data_import, "./output/checked_data_30Jan2021.csv")
+
+data_import<-read_csv("./output/checked_data_30Jan2021.csv") %>%
+  select(-Thumbnail)
 
 ## Clean data ----
 
@@ -26,6 +33,11 @@ data_import3 <- data_import2 %>%
   mutate(actualID = cur_group_id()) %>%
   select(actualID, ID, everything()) %>%
   ungroup()
+
+table_main<-data_import3 %>%
+  select(-contains("designer"), -contains("artist"), -contains("category"), -contains("mechanic"),
+         -contains("family"), -contains("publisher"), -contains("subdomain"))
+# TO DO: clean "expansion to / alternative to" field to refer to ID var.
 
 #EDA ----
 
@@ -55,6 +67,7 @@ get_unique_attribute(attribute="publisher") #2801
 get_unique_attribute(attribute="family") #2104
 get_unique_attribute(attribute="designer") #3974
 get_unique_attribute(attribute="artist") #5154
+get_unique_attribute(attribute="subdomain") #8
 
 create_table <- function(attribute = "") {
   new_table <- data_import3 %>%
@@ -69,12 +82,14 @@ create_table <- function(attribute = "") {
 }
 
 
-artist_table<-create_table(attribute="artist")
-category_table<-create_table(attribute="category")
-designer_table<-create_table(attribute="designer")
-family_table<-create_table(attribute="family")
-mechanic_table<-create_table(attribute="mechanic")
-publisher_table<-create_table(attribute="publisher")
+table_artist<-create_table(attribute="artist")
+table_category<-create_table(attribute="category")
+table_designer<-create_table(attribute="designer")
+table_family<-create_table(attribute="family")
+table_mechanic<-create_table(attribute="mechanic")
+table_publisher<-create_table(attribute="publisher")
+table_subdomain<-create_table(attribute="subdomain")
+
 
 
 get_top_ranked<-function(attribute="", top_x=10){
@@ -101,4 +116,29 @@ get_top_ranked(attribute="designer") #note - uncredited should be coded as NA
 get_top_ranked(attribute="family")
 get_top_ranked(attribute="mechanic")
 get_top_ranked(attribute="publisher")
+get_top_ranked(attribute="subdomain")
 
+#function to return relevant stats to a board game;
+get_attributes<-function(bg_name=""){
+bg_table<-table_main %>%
+  filter(str_to_lower(Name)==str_to_lower(as.character(bg_name))) 
+
+game_ID<-bg_table$ID
+
+game_artists<-table_artist %>%
+  filter(ID==game_ID)
+
+game_designers<-table_designer %>%
+  filter(ID==game_ID)
+
+game_categories<- table_category %>%
+  filter(ID==game_ID)
+
+game_familys<- table_family%>%
+  filter(ID==game_ID)
+  
+
+
+  return(list(bg_table, game_artists, game_familys, game_designers, game_categories))
+}
+apples_to_apples<-get_attributes(bg_name="Apples to apples")
